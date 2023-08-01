41101150b35e157b7e8de23a7859f612d6cf485a
karolcossatis <karolcossatis@gmail.com>
2023-08-01 10:25:42 -0300

    primeiro commit

---
+++ app/views/mensagem-view.ts
@@ -0,0 +1,31 @@
+//export class MensagemView {
+
+ //   private elemento: HTMLElement;
+    
+ //   constructor(seletor: string) {
+ //       this.elemento = document.querySelector(seletor);
+
+ //   }
+
+//    template(model: string): string {
+//        return `
+//        <p class="alert alert-info">${model}</p>
+//        `
+//    }
+
+//    update(model: string): void {
+//        const template = this.template(model);
+//        this.elemento.innerHTML = template; 
+  //  }
+//}
+
+import { View } from "./view.js";
+
+export class MensagemView extends View<string> {
+
+    protected template(model: string): string {
+        return `
+            <p class="alert alert-info"> ${model}</p>
+        `
+    }
+}
---
+++ dist/js/app.js
@@ -0,0 +1,10 @@
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+});
+//const negociacoesView = new NegociacoesView
+//const template = negociacoesView.template();
+//console.log(template);
